c765cfcaafe5c9e19d2b411bbac46cd835b4bb1c
junkratjannek <jannek.kloettschen@gmx.de>
2024-04-22 23:05:22 -0400

    trying around

--- src/app/componentes/travelDestination.tsx
+++ src/app/componentes/travelDestination.tsx
@@ -1,5 +1,5 @@
 interface TravelDestinationProps {}
 
 export default function TravelDestination() {
-  return <div>Travel Destination</div>;
+  return <div className="bg-blue-900 w-max pt-2 pb-2 pl-5 pr-5 rounded full h-max ">Travel Destination</div>;
 }
